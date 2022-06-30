92fef8675ed786ba11e07de3a037bfb3af012d52
Spring Raindrop <SpringRaindrop@vivaldi.net>
2022-06-30 06:02:48 +0000

    Document all public methods with JSDoc

--- src/traverse.js
+++ src/traverse.js
@@ -1,3 +1,7 @@
+/**
+ * The traversal state which the callback will be bound to. Use `this` inside
+ * the callback to access its methods.
+ */
 class TraversalState {
   #ast = null;
   #path = null;
@@ -8,27 +12,44 @@ class TraversalState {
     this.#ast = ast;
   }
 
+  /**
+   * Returns the path to the current node as an array of keys. E. g.: `[ "body", 0, "body", "body", 0, "expression", "callee", "object" ]` is the path to the node at `ast.body[0].body.body[0].expression.callee.object`.
+   */
   get path() {
     return this.#path;
   }
 
+  /**
+   * Returns the current node.
+   */
   get node() {
     return this.getElementAt(this.#path);
   }
 
+  /**
+   * Returns the parent node.
+   */
   get parentNode() {
     return this.getElementAt(getAncestorPath(this.#path));
   }
 
-  // unlike parentNode, might return an array
+  /**
+   * Returns the parent element. The difference between this method and {@link TraversalState#parentNode} is that the latter always returns an AST node, whereas this method may return an array of nodes. E. g. if the current node is located inside `body` of a `BlockStatement`.
+   */
   get parentElement() {
     return this.getElementAt(this.#path.slice(0, -1));
   }
 
+  /**
+   * Returns the last element of the path.
+   */
   get key() {
     return this.#path[this.#path.length - 1];
   }
 
+  /**
+   * Returns an array of ancestor nodes.
+   */
   get ancestors() {
     const ancestors = [];
 
@@ -41,6 +62,10 @@ class TraversalState {
     return ancestors;
   }
 
+  /**
+   * Returns the element at specified path.
+   * @arg {array} path
+   */
   getElementAt(path) {
     let el = this.#ast;
     for (const part of path) el = el[part];
@@ -136,6 +161,9 @@ class TraversalState {
     while (this.#step(callback));
   }
 
+  /**
+   * Skips the children of the current node in the next iteration.
+   */
   skip() {
     this.#nextPath = this.#findNextPath(this.#path, true);
   }
@@ -155,6 +183,11 @@ class TraversalState {
     this.parentElement[this.key] = node;
   }
 
+  /**
+   * Replaces the current node.
+   * @arg {object} node - The new node.
+   * @arg {boolean} skip - If true, skips the new node.
+   */
   replace(node, skip) {
     if (Array.isArray(node)) {
       this.#insertAt(this.#path.slice(0, -1), this.key, node, 1);
@@ -166,6 +199,10 @@ class TraversalState {
     this.#nextPath = this.#findNextPath(this.#path, skip);
   }
 
+  /**
+   * Inserts a node before the current one. Only works if the current node is located inside an array (e. g. inside `body` of a `BlockStatement`).
+   * @arg {object} node - The new node.
+   */
   insertBefore(node) {
     const insertCount = this.#insertAt(
       this.#path.slice(0, -1),
@@ -177,6 +214,11 @@ class TraversalState {
     this.#nextPath = this.#findNextPath(this.#path);
   }
 
+  /**
+   * Inserts a node after the current one. Only works if the current node is located inside an array (e. g. inside `body` of a `BlockStatement`).
+   * @arg {object} node - The new node.
+   * @arg {boolean} skipBoth - If true, skips both the current and the new nodes.
+   */
   insertAfter(node, skipBoth) {
     const insertCount = this.#insertAt(
       this.#path.slice(0, -1),
@@ -231,10 +273,10 @@ function findNextArrayIndex(node, arrayName, currentIndex) {
 }
 
 /**
- * Finds the number of matching elements in two arrays. Returns the number
- * (not the last matching index!). Only works for simple elements, not objects.
+ * Finds the number of matching elements in two arrays. Returns the number (not the last matching index!). Only works for simple elements, not objects.
  * @arg {array} arr1
  * @arg {array} arr2
+ * @private
  */
 function findArrayMatchingLength(arr1, arr2) {
   for (let i = 0; i < arr1.length; i++) {
@@ -248,12 +290,14 @@ function findArrayMatchingLength(arr1, arr2) {
 
 /**
  * Traverses the supplied AST. Goes into every child Node.
- * @arg ast
+ * @arg ast - ESTree-compatible AST to traverse.
  * @arg callback - Function to be called on each node. Is bound to `TraversalState`, so you can use e. g. `this.replace(node)`.
  */
-export default function traverse(ast, callback) {
+function traverse(ast, callback) {
   new TraversalState(ast)._execute(callback);
 }
+
+export default traverse;
 
 // basically, go anywhere there might be nodes, except Location, Identifier, Literal and such
 const propsToVisit = {
